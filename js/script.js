69801902e4674557230b8eec72a4b8251039d1d5
search <samaatarek12z@gmail.com>
2025-05-23 03:22:27 +0300

    add all project files

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var typed = new Typed('#typed', {
+      strings: ["developer"," designer"],
+      typeSpeed: 50,
+      loop:true,
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <div class="carousel-item active">
+      <div class="test d-flex flex-column justify-content-center align-items-center">
+        <i class="fa-solid fa-quote-right fs-1 royal"></i>
+              <img class="d-block w-100 my-4 rounded-circle" src="./images/testi-1.jpg" alt="...">
+
+      </div>
+      <div class="carousel-caption d-none d-md-block text-muted">
+        <p>First slide label</p>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
